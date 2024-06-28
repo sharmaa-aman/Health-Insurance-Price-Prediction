dfa5fb33856d760123e7f19c8be3ad8f6f3a0e40
sharmaa-aman <amansharmaa023@gmail.com>
2024-06-28 19:26:56 +0530

    Creating A Function For Prediction

--- App.py
+++ App.py
@@ -4,3 +4,11 @@ import streamlit as st
 
 #Loading the saved model
 Loaded_Model = pickle.load(open("C:/Users/vinit/Health-Insurance-Price-Prediction/RFR_Model.pkl", 'rb'))
+
+
+# creating a function for Prediction
+def Health_Insuranace_Price_Prediction(age, sex, bmi, children, smoker,region):
+    
+    prediction = Loaded_Model.predict([[age, sex, bmi, children, smoker, region]])
+    print(prediction)
+    return prediction
